5ba58657f814e5e0e7f6cd9abb1d9b286ebd11a6
Daosheng Mu <daoshengmu@gmail.com>
2015-05-25 10:07:50 +0800

    Update DeviceOrientationControls.js

--- js/threejs/DeviceOrientationControls.js
+++ js/threejs/DeviceOrientationControls.js
@@ -44,13 +44,13 @@ THREE.DeviceOrientationControls = function ( object ) {
 		return function ( quaternion, alpha, beta, gamma, orient ) {
 
 			//euler.set( beta, alpha, - gamma, 'YXZ' );                     // 'ZXY' for the device, but 'YXZ' for us
-			euler.set( gamma, alpha, beta, 'YXZ' );							// Modify for fit Cardboard demo, @daoshengmu
+			euler.set( gamma, alpha, beta, 'YXZ' );				// Modify for fit Cardboard demo, @daoshengmu
 
 			quaternion.setFromEuler( euler );                               // orient the device
 
 			quaternion.multiply( q1 );                                      // camera looks out the back of the device, not the top
 
-			quaternion.multiply( q0.setFromAxisAngle( zee, -orient ) );    // adjust for screen orientation
+			quaternion.multiply( q0.setFromAxisAngle( zee, -orient ) );     // adjust for screen orientation
 
 		}
 
